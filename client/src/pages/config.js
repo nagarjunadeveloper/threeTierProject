51589ec2aa21466b28555ee9aaaff11853e96c45
abhi1563 <abhijeet.kumar1563@gmail.com>
2025-08-02 15:44:44 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -2,6 +2,6 @@
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 // export default API_BASE_URL;
-const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
+const API_BASE_URL = "http;//13.221.97.134:86";
 export default API_BASE_URL;
 
