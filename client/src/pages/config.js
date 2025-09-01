a99ca763d318f7d94aada87aab81cb194c5a3e60
nagarjunadeveloper <140618588+nagarjunadeveloper@users.noreply.github.com>
2025-09-01 19:01:39 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -2,6 +2,6 @@
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 // export default API_BASE_URL;
-const API_BASE_URL = "http://3.80.133.53:86";
+const API_BASE_URL = "http://3.90.42.189:86";
 export default API_BASE_URL;
 
